9c213faa826e69f14096081bb395ba82b0dcd99d
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-02-09 17:08:02 +0800

    Update 0017. A×B Problem.cpp

--- C++/0017. A×B Problem.cpp
+++ C++/0017. A×B Problem.cpp
@@ -1,5 +1,7 @@
 // we have defined the necessary header files here for this problem.
 // If additional header files are needed in your program, please import here.
+
+/*
 #include<string.h>
 
 string string_plus(string s1,string s2){
@@ -78,6 +80,40 @@ int main()
         }
 
         cout<<res<<endl;
+    }
+    return 0;
+}
+*/
+
+#include<string.h>
+
+int main()
+{  
+  // please define the C++ input here. For example: int a,b; cin>>a>>b;;  
+  // please finish the function body here.  
+  // please define the C++ output here. For example:cout<<____<<endl; 
+    string s1,s2;
+    
+    while(cin>>s1>>s2){    
+        int l1 = s1.length();
+        int l2 = s2.length();
+        string res(l1+l2,0);
+        
+        for(int i=l1-1;i>=0;--i){
+            for(int j=l2-1;j>=0;--j){
+                res[i+j+1] += (s1[i]-'0')*(s2[j]-'0');
+                res[i+j] += res[i+j+1]/10;
+                res[i+j+1] %= 10;
+            }
+        }
+        
+        for(auto& c:res){
+            c += '0';
+        }
+        
+        size_t idx = res.find_first_not_of('0');
+
+        cout<<(idx==string::npos?"0":res.substr(idx))<<endl;
     }
     return 0;
 }
