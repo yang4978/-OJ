951871da8647faa70dc4386b24870c601102530e
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-02-19 17:19:20 +0800

    Create 0023. 最大公约数 Plus.cpp

---
+++ C++/0023. 最大公约数 Plus.cpp
@@ -0,0 +1,14 @@
+// we have defined the necessary header files here for this problem.
+// If additional header files are needed in your program, please import here.
+
+int main()
+{  
+  // please define the C++ input here. For example: int a,b; cin>>a>>b;;  
+  // please finish the function body here.  
+  // please define the C++ output here. For example:cout<<____<<endl; 
+    int n;
+    while(cin>>n){
+        cout<<(2*n & -2*n)<<endl;
+    }
+    return 0;
+}
