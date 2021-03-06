a1f05ec8c375029062fdfc4362b2717a1fc9cbcb
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-03-06 09:14:52 +0800

    Create 0019. N! Plus.py

---
+++ Python/0019. N! Plus.py
@@ -0,0 +1,20 @@
+# If you need to import additional packages or classes, please import here.
+
+def func():
+    # please define the python3 input here. 
+    # For example: a,b = map(int, input().strip().split())
+    # please finish the function body here.
+    # please define the python3 output here. For example: print().
+    while True:
+        try:
+            n = int(input())
+            res = 1
+            while n > 1:
+                res *= n
+                n -= 1
+            print(res)
+        except EOFError:
+            break
+    
+if __name__ == "__main__":
+    func()
