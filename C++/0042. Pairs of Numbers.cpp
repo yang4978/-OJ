c57ce8ee6c0863d2b8e4ac4b27e9d5073ca115ed
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-03-06 15:43:12 +0800

    Create 0042. Pairs of Numbers.cpp

---
+++ C++/0042. Pairs of Numbers.cpp
@@ -0,0 +1,32 @@
+// we have defined the necessary header files here for this problem.
+// If additional header files are needed in your program, please import here.
+int k;
+int recursive(int a, int b){
+    if(b>a){
+        swap(a,b);
+    }
+    if(b==0){
+        return k;
+    }
+    if(b==1){
+        return a-1;
+    }
+    return recursive(a%b,b)+a/b;
+}
+
+int main()
+{  
+  // please define the C++ input here. For example: int a,b; cin>>a>>b;;  
+  // please finish the function body here.  
+  // please define the C++ output here. For example:cout<<____<<endl; 
+    while(cin>>k){
+        int res = (1<<31)-1;
+        
+        for(int i=1;i<=k;++i){
+            res = min(res,recursive(k,i));
+        }
+        
+        cout<<res<<endl;   
+    }
+    return 0;
+}
