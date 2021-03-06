6cf4118b963caa066ae24e01e63c36174c50bb04
yang4978 <40636563+yang4978@users.noreply.github.com>
2021-03-06 17:31:17 +0800

    Create 0046. 全排列.py

---
+++ Python/0046. 全排列.py
@@ -0,0 +1,32 @@
+# If you need to import additional packages or classes, please import here.
+
+def func():
+    # please define the python3 input here. 
+    # For example: a,b = map(int, input().strip().split())
+    # please finish the function body here.
+    # please define the python3 output here. For example: print().
+    while True:
+        try:
+            def dfs(index,used,arr,l):
+                if l == n:
+                    res.append(arr)
+                    return
+                for i in range(1,n+1):
+                    if used[i] == 0:
+                        used[i] = 1
+                        dfs(i,used,arr+[str(i)],l+1)
+                        used[i] = 0
+                
+            res = []
+            n= int(input())
+            used = [0]*(n+1)
+            dfs(0,used,[],0)
+            
+            for l in res:
+                print((' ').join(l))
+            
+        except EOFError:
+            break
+    
+if __name__ == "__main__":
+    func()
